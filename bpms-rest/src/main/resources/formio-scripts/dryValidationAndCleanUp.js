21f241941f991bb2af6f54f75517caca6c9882ca
aberezovski <Artem.Berezovski@artezio.com>
2019-12-31 11:07:12 +0300

    Fixed bug with undefined Option component

--- bpms-rest/src/main/resources/formio-scripts/dryValidationAndCleanUp.js
+++ bpms-rest/src/main/resources/formio-scripts/dryValidationAndCleanUp.js
@@ -1,13 +1,16 @@
 require('jsdom-global')();
-const { Formio } = require('formiojs');
+const {Formio} = require('formiojs');
+global.Option = global.window.Option;
 
 const body = document.body;
 const [form, submission] = process.argv.slice(1).map(JSON.parse);
 
 Formio.createForm(body, form)
     .then(instance => {
-        instance.on('error', error => { console.error(error) });
-        instance.on('submit', submit => { console.info(JSON.stringify(submit)) });
-        instance.on('change', () => instance.submit());
+        instance.on('change',
+            () => instance.submit()
+                .then(
+                    submit => console.info('submit:', submit),
+                    error => console.error('error:', error)));
         instance.submission = submission;
     });
